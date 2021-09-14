d2e701bcc624b5e6c81a9ffa8a2b0e6f0503078d
Lumm1t <aleksander.szolowicz@gmail.com>
2021-09-14 11:36:21 +0200

    feat(server): scrap locations options

--- server/controllers/scraper.ts
+++ server/controllers/scraper.ts
@@ -14,7 +14,6 @@ nightmare
 const getLocation = (inputName: string): Promise<string[]> => {
   return new Promise((resolve, reject) => {
     nightmare
-      .wait('form#SearchSubjectForm')
       // @ts-ignore
       .evaluate((inputName: string) => {
         // @ts-ignore
@@ -27,33 +26,82 @@ const getLocation = (inputName: string): Promise<string[]> => {
   })
 }
 
+const setLocation = (inputName: string, value: string): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    nightmare
+      .select(`select[id*='${inputName}']`, value)
+      .then(async () => {
+        await clickSearchButton()
+
+        resolve()
+      })
+      .catch(reject)
+  })
+}
+
+const clickSearchButton = (): Promise<void> => {
+  return new Promise((resolve, reject) => {
+    nightmare
+      .click('input[value=Szukaj]')
+      .evaluate(() => document.querySelector('form#SearchSubjectForm').remove())
+      .wait('form#SearchSubjectForm')
+      .then(resolve)
+      .catch(reject)
+  })
+}
+
 const locationThread = async (ctx: any) => {
-  const locationsInfo = [
-    {
-      name: 'state',
-      inputName: 'SearchStateId',
-    },
-    // {
-    //   name: 'substate',
-    //   inputName: 'SearchSubstateId',
-    // },
-    // {
-    //   name: 'community',
-    //   inputName: 'SearchCommunityName',
-    // },
-    // {
-    //   name: 'entity',
-    //   inputName: 'SearchAclgroupId',
-    // },
+  const params = ctx.request.body
+
+  const selectedLocations: string[] = params.selectedLocations
+
+  let searchIndex = 0
+
+  const availableLocations = [
+    'SearchStateId',
+    'SearchSubstateId',
+    'SearchCommunityName',
+    'SearchAclgroupId',
   ]
 
-  const possibleLocations = {}
-  for (const { name, inputName } of locationsInfo) {
-    // @ts-ignore
-    answer[`${name}`] = await getLocation(inputName)
+  if (!selectedLocations) {
+    return (ctx.body = {
+      success: false,
+      error: 'NO_PARAM',
+    })
   }
 
-  ctx.body = possibleLocations
+  if (!Array.isArray(selectedLocations)) {
+    return (ctx.body = {
+      success: false,
+      error: 'INVALID_PARAM',
+    })
+  }
+
+  if (selectedLocations.length >= availableLocations.length) {
+    return (ctx.body = {
+      success: false,
+      error: 'TOO_MANY_PARAMS',
+    })
+  }
+
+  if (selectedLocations.length === 0) {
+    return (ctx.body = {
+      success: true,
+      response: await getLocation(availableLocations[searchIndex]),
+    })
+  }
+
+  for (const selectedLocation of selectedLocations) {
+    await setLocation(availableLocations[searchIndex], selectedLocation)
+
+    ++searchIndex
+  }
+
+  ctx.body = {
+    success: true,
+    response: await getLocation(availableLocations[searchIndex]),
+  }
 }
 
 const getSubjects = (ctx: any) => {
