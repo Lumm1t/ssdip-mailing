12aea9c24a42f14d8e3c6d0af2300d02005c4022
Lumm1t <aleksander.szolowicz@gmail.com>
2021-09-15 10:13:21 +0200

    feat(client): add locations form

---
+++ client/components/LocationSelect.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-select
+    v-model="selectedLocation"
+    :disabled="options.length === 0"
+    item-text="text"
+    item-value="value"
+    :label="name"
+    :items="options"
+    required
+    @input="emitSelectedLocation"
+  ></v-select>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+
+export default Vue.extend({
+  name: 'LocationSelect',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    options: {
+      type: Array as PropType<string[]>,
+      required: true,
+    },
+    index: {
+      type: Number,
+      required: true,
+    },
+  },
+  data: () => ({
+    selectedLocation: undefined,
+  }),
+  methods: {
+    emitSelectedLocation() {
+      this.$emit('selected-location', this.index, this.selectedLocation)
+    },
+  },
+})
+</script>
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,3 +1,77 @@
 <template>
-  <v-alert dense text type="success"> Hello world! </v-alert>
+  <v-form>
+    <LocationSelect
+      v-for="(location, i) in availableLocations"
+      :key="location.name"
+      :name="location.name"
+      :options="location.options"
+      :index="i"
+      @selected-location="updateSelect"
+    />
+  </v-form>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data: () => ({
+    selectedLocations: [],
+    availableLocations: [
+      {
+        name: 'state',
+        options: [],
+      },
+      {
+        name: 'substate',
+        options: [],
+      },
+      {
+        name: 'community',
+        options: [],
+      },
+      {
+        name: 'entity',
+        options: [],
+      },
+    ],
+  }),
+  async mounted() {
+    await this.updateSelect(-1, 'init')
+  },
+  methods: {
+    updateSelect(...args: any[]) {
+      const [index, selectedLocation] = args
+
+      if (selectedLocation !== 'init') {
+        this.resetSelects(index)
+
+        this.selectedLocations[index] = selectedLocation || ''
+      }
+
+      if (this.selectedLocations.length < this.availableLocations.length)
+        this.getAvailableLocations(index)
+    },
+    getAvailableLocations(index: number) {
+      this.$axios
+        .post('/scraper/locations', {
+          selectedLocations: this.selectedLocations,
+        })
+        .then(data => {
+          if (data.data.success) {
+            this.availableLocations[index + 1].options = data.data.response
+          } else alert(data.data.error)
+        })
+    },
+    resetSelects(properPosition: number) {
+      this.selectedLocations.length = properPosition + 1
+
+      for (const [index, location] of this.availableLocations.entries()) {
+        if (index > properPosition) {
+          location.options = []
+        }
+      }
+    },
+  },
+})
+</script>
