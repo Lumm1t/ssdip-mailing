99d26b25aeeda9d6ac3547455f4004f66fc7ebf5
Lumm1t <aleksander.szolowicz@gmail.com>
2021-09-16 08:46:26 +0200

    feat(location form): move to separated file

---
+++ client/components/LocationForm.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <LocationSelect
+      v-for="(location, i) in availableLocations"
+      :key="location.name"
+      :name="location.name"
+      :options="location.options"
+      :index="i"
+      @selected-location="updateSelect"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import { selectedLocations } from '../store'
+
+export default Vue.extend({
+  data: () => ({
+    availableLocations: [
+      {
+        name: 'state',
+        options: [],
+      },
+      {
+        name: 'substate',
+        options: [],
+      },
+      {
+        name: 'community',
+        options: [],
+      },
+      {
+        name: 'entity',
+        options: [],
+      },
+    ],
+  }),
+  async mounted() {
+    await this.updateSelect(-1, 'init')
+  },
+  methods: {
+    updateSelect(...args: any[]) {
+      const [index, selectedLocation] = args
+
+      if (selectedLocation !== 'init') {
+        this.resetSelects(index)
+
+        selectedLocations.update({ index, selectedLocation })
+      }
+
+      if (selectedLocations.length < this.availableLocations.length) {
+        this.getAvailableLocations(index)
+      }
+    },
+    getAvailableLocations(index: number) {
+      this.$axios
+        .post('/scraper/locations', {
+          selectedLocations: selectedLocations.locations,
+        })
+        .then(data => {
+          if (data.data.success) {
+            this.availableLocations[index + 1].options = data.data.response
+          } else alert(data.data.error)
+        })
+    },
+    resetSelects(properPosition: number) {
+      selectedLocations.resetDependent(properPosition + 1)
+
+      for (const [index, location] of this.availableLocations.entries()) {
+        if (index > properPosition) {
+          location.options = []
+        }
+      }
+    },
+  },
+})
+</script>
