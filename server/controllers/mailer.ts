98bee4c96772ea11c8158c8e6d66034a1d6373dc
Lumm1t <aleksander.szolowicz@gmail.com>
2021-09-17 09:20:12 +0200

    feat(server): add params validation to mailer

--- server/controllers/mailer.ts
+++ server/controllers/mailer.ts
@@ -1,6 +1,12 @@
 import '../env'
 import nodemailer from 'nodemailer'
 
+interface Params {
+  recipients: string[]
+  subject: string
+  body: string
+}
+
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   auth: {
@@ -13,6 +19,35 @@ transporter.verify(err => console.log(err || 'nodemailer ready'))
 
 const mailThread = (ctx: any) => {
   const params = ctx.request.body
+
+  const paramsLength = Object.keys(params).length
+
+  const { recipients, subject, body }: Params = params
+
+  if (!recipients || !subject || !body) {
+    return (ctx.body = {
+      success: false,
+      error: 'NO_PARAM',
+    })
+  }
+
+  if (
+    !Array.isArray(recipients) ||
+    typeof subject !== 'string' ||
+    typeof body !== 'string'
+  ) {
+    return (ctx.body = {
+      success: false,
+      error: 'INVALID_PARAM',
+    })
+  }
+
+  if (paramsLength > 3) {
+    return (ctx.body = {
+      success: false,
+      error: 'TOO_MANY_PARAMS',
+    })
+  }
 
   ctx.body = {
     success: true,
