f2f8543053f3eadac217e8e1af41e295b73fdb86
Lumm1t <aleksander.szolowicz@gmail.com>
2021-09-17 08:42:40 +0200

    feat(server): use env for smtp

--- server/controllers/mailer.ts
+++ server/controllers/mailer.ts
@@ -1,3 +1,16 @@
+import '../env'
+import nodemailer from 'nodemailer'
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+})
+
+transporter.verify(err => console.log(err || 'nodemailer ready'))
+
 const mailThread = (ctx: any) => {
   const params = ctx.request.body
 
