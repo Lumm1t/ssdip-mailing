88350082abf1224c33f8088f3305a10c6f480f2c
Lumm1t <aleksander.szolowicz@gmail.com>
2021-09-29 12:12:15 +0200

    feat(client): add theme button

---
+++ client/components/ThemeBtn.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-tooltip bottom>
+    <template #activator="{ on }">
+      <v-btn icon @click="changeTheme" v-on="on">
+        <v-icon>mdi-brightness-6</v-icon>
+      </v-btn>
+    </template>
+
+    <span>Change theme</span>
+  </v-tooltip>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { global } from '../store'
+import setupTheme from '~/assets/setup-theme'
+
+export default Vue.extend({
+  name: 'ThemeBtn',
+  mounted() {
+    setupTheme.call(this)
+  },
+  methods: {
+    changeTheme() {
+      global.changeTheme()
+    },
+  },
+})
+</script>
