34758c64667fa0b38f61c5ae8382239f0061e6fe
Lumm1t <aleksander.szolowicz@gmail.com>
2021-09-29 12:10:15 +0200

    feat(assets): subscribe to change-theme mutation

---
+++ client/assets/setup-theme.ts
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import { Mutation, State } from '~/client/types/store'
+
+export default function setupTheme(this: Vue) {
+  this.$vuetify.theme.dark = this.$store.state.global.darkTheme
+
+  this.$store.subscribe(({ type }: Mutation, state: State) => {
+    if (type === 'global/changeTheme') {
+      this.$vuetify.theme.dark = state.global.darkTheme
+    }
+  })
+}
