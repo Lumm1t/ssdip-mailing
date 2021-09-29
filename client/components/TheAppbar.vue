b6b1808f3f7c4619a33f26fc7d2e80500f710ac3
Lumm1t <aleksander.szolowicz@gmail.com>
2021-09-29 12:12:40 +0200

    feat(client): add app bar

---
+++ client/components/TheAppbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-app-bar elevate-on-scroll>
+      <v-spacer></v-spacer>
+
+      <ThemeBtn />
+    </v-app-bar>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'TheAppbar',
+})
+</script>
